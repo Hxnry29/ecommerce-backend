a3c7a1158b62528dcdc1738453ad01c9e52b9c31
Video Juegos <gamedevlab@o365.unab.edu.co>
2025-04-22 14:30:33 -0500

    Ajuste para preparar el proyecto para laravel ui auth

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,22 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        .form-container {
-            background-color: #ffffff;
-            padding: 2rem;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        }
-        .btn-primary {
-            background-color: #fcb900;
-            border-color: #fcb900;
-        }
-        .btn-primary:hover {
-            background-color: #e0a800;
-            border-color: #e0a800;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset ('css/style.css')}}">
+
 </head>
 <body>
 
